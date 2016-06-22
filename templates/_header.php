2b1351251ad8b4772c76e8e689bcbd0d879e2204
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-06-22 13:08:10 -0400

    test

--- templates/_header.php
+++ templates/_header.php
@@ -1,61 +1,81 @@
+<?php echo get_component_slot('header') ?>
+
 <?php echo get_component('default', 'updateCheck') ?>
 
-<?php if ($sf_user->isAdministrator() && (string)QubitSetting::getByName('siteBaseUrl') === ''): ?>
-  <div id="update-check">
-    <?php echo link_to('Please configure your site base URL', 'settings/siteInformation', array('rel' => 'home', 'title' => __('Home'))) ?>
+<?php if ($sf_user->isAuthenticated()): ?>
+  <div id="top-bar">
+    <nav>
+      <?php echo get_component('menu', 'userMenu') ?>
+      <?php echo get_component('menu', 'quickLinksMenu') ?>
+      <?php echo get_component('menu', 'changeLanguageMenu') ?>
+      <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+    </nav>
   </div>
 <?php endif; ?>
 
-<header id="top-bar">
+<div id="header">
 
-  <?php if (sfConfig::get('app_toggleLogo')): ?>
-    <?php echo link_to(image_tag('/plugins/arSparcPlugin/images/sparc.png', array('alt' => 'FIT Special Collections and College Archives')), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
-  <?php endif; ?>
+  <div class="container">
 
-  <?php if (sfConfig::get('app_toggleTitle')): ?>
-    <h1 id="site-name">
-      <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
-    </h1>
-  <?php endif; ?>
-
-  <nav>
-
-    <?php echo get_component('menu', 'userMenu') ?>
-
-    <?php echo get_component('menu', 'quickLinksMenu') ?>
-
-    <?php if (sfConfig::get('app_toggleLanguageMenu')): ?>
-      <?php echo get_component('menu', 'changeLanguageMenu') ?>
-    <?php endif; ?>
-
-    <?php echo get_component('menu', 'clipboardMenu') ?>
-
-    <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-
-  </nav>
-
-  <div id="search-bar">
-
-    <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-
-    <?php echo get_component('search', 'box') ?>
-
-  </div>
-
-  </section>
-
-  <?php echo get_component_slot('header') ?>
-
-</header>
-
-<?php if (sfConfig::get('app_toggleDescription')): ?>
-  <div id="site-slogan">
-    <div class="container">
+    <div id="header-lvl1">
       <div class="row">
         <div class="span12">
-          <span><?php echo esc_specialchars(sfConfig::get('app_siteDescription')) ?></span>
+
+          <?php if ('fr' == $sf_user->getCulture()): ?>
+            <a id="header-council" href="http://cdncouncilarchives.ca"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.fr.png', array('width' => '156', 'height' => '42', 'alt' => __('Canadian Council of Archives'))) ?></a>
+          <?php else: ?>
+            <a id="header-council" href="http://cdncouncilarchives.ca"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.en.png', array('width' => '156', 'height' => '42', 'alt' => __('Canadian Council of Archives'))) ?></a>
+          <?php endif; ?>
+
+          <ul id="header-nav" class="nav nav-pills">
+
+            <?php if ('fr' == $sf_user->getCulture()): ?>
+              <li><?php echo link_to(__('Home'), 'http://archivescanada.ca/homeFR') ?></li>
+            <?php else: ?>
+              <li><?php echo link_to(__('Home'), 'http://archivescanada.ca') ?></li>
+            <?php endif; ?>
+
+            <?php if ('fr' == $sf_user->getCulture()): ?>
+              <li><?php echo link_to(__('Contactez-nous'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php else: ?>
+              <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php endif; ?>
+
+            <?php foreach (array('en', 'fr') as $item): ?>
+              <?php if ($sf_user->getCulture() != $item): ?>
+                <li><?php echo link_to(format_language($item, $item), array('sf_culture' => $item) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?></li>
+                <?php break; ?>
+              <?php endif; ?>
+            <?php endforeach; ?>
+
+            <?php if (!$sf_user->isAuthenticated()): ?>
+              <li><?php echo link_to(__('Log in'), array('module' => 'user', 'action' => 'login')) ?></li>
+            <?php endif; ?>
+
+          </ul>
+
         </div>
       </div>
     </div>
+
+    <div id="header-lvl2">
+      <div class="row">
+
+        <div id="logo-and-name" class="span6">
+          <?php if ('fr' == $sf_user->getCulture()): ?>
+            <h1><?php echo link_to(image_tag('/plugins/arArchivesCanadaPlugin/images/logo.png', array('alt' => __('Archives Canada'))), 'http://archivescanada.ca/homeFR', array('rel' => 'home')) ?></h1>
+          <?php else: ?>
+            <h1><?php echo link_to(image_tag('/plugins/arArchivesCanadaPlugin/images/logo.png', array('alt' => __('Archives Canada'))), 'http://archivescanada.ca', array('rel' => 'home')) ?></h1>
+          <?php endif; ?>
+        </div>
+
+        <div id="header-search" class="span6">
+          <?php echo get_component('search', 'box') ?>
+        </div>
+
+      </div>
+    </div>
+
   </div>
-<?php endif; ?>
+
+</div>
