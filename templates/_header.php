5929ad06748bb4c5f50d060f056d1337139a9e57
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-06-29 15:59:17 -0400

    header

--- templates/_header.php
+++ templates/_header.php
@@ -7,41 +7,41 @@
 <?php endif; ?>
 
 <header id="top-bar">
+	<div class="container">
+	  <?php if (sfConfig::get('app_toggleLogo')): ?>
+		<?php echo link_to(image_tag('/plugins/arSparcPlugin/images/sparc.png', array('alt' => __('FIT Special Collections and College Archives'))), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
+	  <?php endif; ?>
 
-  <?php if (sfConfig::get('app_toggleLogo')): ?>
-    <?php echo link_to(image_tag('/plugins/arSparcPlugin/images/sparc.png', array('alt' => __('FIT Special Collections and College Archives'))), '@homepage', array('id' => 'logo', 'rel' => 'home')) ?>
-  <?php endif; ?>
+	  <?php if (sfConfig::get('app_toggleTitle')): ?>
+		<h1 id="site-name">
+		  <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
+		</h1>
+	  <?php endif; ?>
 
-  <?php if (sfConfig::get('app_toggleTitle')): ?>
-    <h1 id="site-name">
-      <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?>
-    </h1>
-  <?php endif; ?>
+	  <nav>
 
-  <nav>
+		<?php echo get_component('menu', 'userMenu') ?>
 
-    <?php echo get_component('menu', 'userMenu') ?>
+		<?php echo get_component('menu', 'quickLinksMenu') ?>
 
-    <?php echo get_component('menu', 'quickLinksMenu') ?>
+		<?php echo get_component('menu', 'changeLanguageMenu') ?>
 
-    <?php echo get_component('menu', 'changeLanguageMenu') ?>
+		<?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
 
-    <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+	  </nav>
 
-  </nav>
+	  <div id="search-bar">
 
-  <div id="search-bar">
+		<?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
 
-    <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
+		<?php echo get_component('search', 'box') ?>
 
-    <?php echo get_component('search', 'box') ?>
+	  </div>
 
-  </div>
+	  </section>
 
-  </section>
-
-  <?php echo get_component_slot('header') ?>
-
+	  <?php echo get_component_slot('header') ?>
+	</div>
 </header>
 
 <?php if (sfConfig::get('app_toggleDescription')): ?>
