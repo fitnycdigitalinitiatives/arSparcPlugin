1909dda7805b11b3e13aa0285b85e79b1e46ceff
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-06 17:13:47 -0400

    search bar

--- templates/_header.php
+++ templates/_header.php
@@ -16,7 +16,7 @@
 			  <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
 			  <?php echo get_component('menu', 'userMenu') ?>
 			</ul>
-			<form class="navbar-search pull-right">
+			<form class="navbar-search pull-left">
 				<input type="text" class="search-query" placeholder="Search">
 			</form>
 	  </div>
