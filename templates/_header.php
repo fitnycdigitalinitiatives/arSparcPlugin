402b975b942ff681413b3f53df389cdfae62f6a4
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-07 11:54:40 -0400

    spanish

--- templates/_header.php
+++ templates/_header.php
@@ -25,7 +25,8 @@
 			  <?php echo get_component('menu', 'changeLanguageMenu') ?>
 			  <?php if ('fr' == $sf_user->getCulture()): ?>
 				<li><?php echo link_to(__('À propos'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
-			  
+			  <?php elseif ('es' == $sf_user->getCulture()): ?>
+				<li><?php echo link_to(__('Acera'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
 			  <?php else: ?>
 			  <li><?php echo link_to(__('About'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
 			  <?php endif; ?>
