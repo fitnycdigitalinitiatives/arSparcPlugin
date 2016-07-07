4281c71062d9fcf39e50a9dbd3bc579fb7aab264
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-07 11:52:44 -0400

    testing

--- templates/_header.php
+++ templates/_header.php
@@ -25,12 +25,7 @@
 			  <?php echo get_component('menu', 'changeLanguageMenu') ?>
 			  <?php if ('fr' == $sf_user->getCulture()): ?>
 				<li><?php echo link_to(__('À propos'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
-			  <?php elseif ('es' == $sf_user->getCulture(): ?>
-			    <li><?php echo link_to(__('Acera'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
-			  <?php elseif ('nl' == $sf_user->getCulture(): ?>
-			    <li><?php echo link_to(__('Over'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
-			  <?php elseif ('pt' == $sf_user->getCulture(): ?>
-			    <li><?php echo link_to(__('Sobre'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
+			  
 			  <?php else: ?>
 			  <li><?php echo link_to(__('About'), array('module' => 'staticpage', 'slug' => 'about')) ?></li>
 			  <?php endif; ?>
