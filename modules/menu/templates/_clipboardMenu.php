dba1c308eaec6bace045d195668ad4d15396717d
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-26 16:54:15 -0400

    Revert "testing"
    This reverts commit f15653768dca5628a4119010a3ab43473312f852.

--- modules/menu/templates/_clipboardMenu.php
+++ modules/menu/templates/_clipboardMenu.php
@@ -1,8 +1,4 @@
-<button class="top-item" data-toggle="dropdown" data-target="#" aria-expanded="false" data-alert-message="<?php echo __('Note: clipboard items unclipped in this page will be removed from the clipboard when the page is refreshed. You can re-select them now, or reload the page to remove them completely. Using the sort or print preview buttons will also cause a page reload - so anything currently deselected will be lost!') ?>">
-    <a href="/user/clipboard">
-	<?php echo __('Clipboard') ?>
-    <?php if ($count > 0): ?>
-      <span><?php echo $count ?></span>
-    <?php endif; ?>
-	</a>
-</button>
+<li class="dropdown" id="clipboard-menu" data-alert-message="<?php echo __('Note: clipboard items unclipped in this page will be removed from the clipboard when the page is refreshed. You can re-select them now, or reload the page to remove them completely. Using the sort or print preview buttons will also cause a page reload - so anything currently deselected will be lost!') ?>">
+
+	  <a href="/user/clipboard"><?php echo __('Clipboard') ?><?php if ($count > 0): ?> <span class="badge badge-info"><?php echo $count ?></span><?php endif; ?></a>
+</li>
