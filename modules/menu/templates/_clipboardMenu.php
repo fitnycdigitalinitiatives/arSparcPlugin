4953a90d41fef5bf69395b903765af0cfe63fcbf
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-26 10:41:29 -0400

    added clipboard

---
+++ modules/menu/templates/_clipboardMenu.php
@@ -0,0 +1,16 @@
+<li class="dropdown" id="clipboard-menu" data-alert-message="<?php echo __('Note: clipboard items unclipped in this page will be removed from the clipboard when the page is refreshed. You can re-select them now, or reload the page to remove them completely. Using the sort or print preview buttons will also cause a page reload - so anything currently deselected will be lost!')>
+
+	  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('Clipboard') ?><b class="caret"></b><?php if ($count > 0): ?><span class="badge badge-info"><?php echo $count ?></span><?php endif; ?></a>
+
+	  <ul class="dropdown-menu">
+			<li>
+			<?php if ($count != 1): ?>
+			  <?php echo $count . ' ' . __('descriptions selected') ?>
+			<?php else: ?>
+			  <?php echo $count . ' ' . __('description selected') ?>
+			<?php endif; ?>
+			</li>
+			<li class="divider"></li>
+			<?php echo QubitMenu::displayHierarchyAsList($menu, 0, array('overrideVisibility' => array('admin' => $sf_user->isAdministrator()))) ?>
+	  </ul>
+</li>
