3a5081342c231008d81cbaaa80e3b497d4470991
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-28 14:51:12 -0400

    home page

---
+++ modules/staticpage/templates/homeSuccess.php
@@ -0,0 +1,16 @@
+<?php decorate_with('layout') ?>
+
+
+<div class="page">
+  <?php echo render_value($sf_data->getRaw('content')) ?>
+</div>
+
+<?php if (QubitAcl::check($resource, 'update')): ?>
+  <?php slot('after-content') ?>
+    <section class="actions">
+      <ul>
+        <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'), 'class' => 'c-btn')) ?></li>
+      </ul>
+    </section>
+  <?php end_slot() ?>
+<?php endif; ?>
