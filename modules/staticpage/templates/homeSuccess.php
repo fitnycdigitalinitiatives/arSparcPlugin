34c2137f5b41b5434523024b28d33731c6a25773
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-28 15:13:13 -0400

    direct insert of carousel

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -3,7 +3,38 @@
 	<h1><?php echo render_title($resource->getTitle(array('cultureFallback' => true))) ?></h1>
   <?php end_slot() ?>
 
-  <?php echo render_value($sf_data->getRaw('content')) ?>
+  <div id="myCarousel" class="carousel slide">
+	<ol class="carousel-indicators">
+	  <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+	  <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+	  <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+	</ol>
+	<div class="carousel-inner">
+	  <div class="item">
+		<img src="https://fit.vrchost.com/media/get/78226/lb_sc_000133/" alt="">
+		<div class="carousel-caption">
+		  <h4>First Thumbnail label</h4>
+		  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		</div>
+	  </div>
+	  <div class="item active">
+		<img src="https://fit.vrchost.com/media/get/78226/lb_sc_000133/" alt="">
+		<div class="carousel-caption">
+		  <h4>Second Thumbnail label</h4>
+		  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		</div>
+	  </div>
+	  <div class="item">
+		<img src="https://fit.vrchost.com/media/get/78226/lb_sc_000133/" alt="">
+		<div class="carousel-caption">
+		  <h4>Third Thumbnail label</h4>
+		  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		</div>
+	  </div>
+	</div>
+	<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
+	<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
+</div>
 
 
 <?php if (QubitAcl::check($resource, 'update')): ?>
