1743cbbe89f4ae095d8bd06b14c872fa2a83cb4f
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-28 15:09:46 -0400

    remove slot

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -7,11 +7,11 @@
 
 
 <?php if (QubitAcl::check($resource, 'update')): ?>
-  <?php slot('after-content') ?>
+
     <section class="actions">
       <ul>
         <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'), 'class' => 'c-btn')) ?></li>
       </ul>
     </section>
-  <?php end_slot() ?>
+
 <?php endif; ?>
