5cc7d00233e352b1a608129978a8cc68320a232f
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-10-13 11:28:27 -0400

    footer

---
+++ templates/_footer.php
@@ -0,0 +1,32 @@
+<footer>
+
+  <?php if (QubitAcl::check('userInterface', 'translate')): ?>
+    <?php echo get_component('sfTranslatePlugin', 'translate') ?>
+  <?php endif; ?>
+  <div class="container">
+    <?php echo get_component_slot('footer') ?>
+    <div class="span3">
+      <?php echo link_to(image_tag('/plugins/arSparcPlugin/images/FITSUNY1_white.png', array('alt' => __('Fashion Institute of Technology - State University of New York'))), 'http://www.fitnyc.edu/') ?>
+    </div>
+  </div>
+
+
+  <div id="print-date">
+    <?php echo __('Printed: %d%', array('%d%' => date('Y-m-d'))) ?>
+  </div>
+
+</footer>
+
+<?php if (null !== $gaKey = sfConfig::get('app_google_analytics_api_key')): ?>
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '<?php echo $gaKey ?>']);
+    _gaq.push(['_trackPageview']);
+    <?php include_slot('google_analytics') ?>
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
+<?php endif; ?>
