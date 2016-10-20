be4d4887fa6c9ff05546d6659323b88505bcf372
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-10-20 16:43:02 -0400

    search

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -7,7 +7,14 @@
   	</div>
   	<div class="row" id="explore-dropdown">
   		<div class="span12">
-  			<?php echo get_component('search', 'box') ?>
+        <div id="search-form-wrapper" role="search">
+          <h2><?php echo __('Search') ?></h2>
+          <form action="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse')) ?>" data-autocomplete="<?php echo url_for(array('module' => 'search', 'action' => 'autocomplete')) ?>" autocomplete="off">
+            <input type="hidden" name="topLod" value="0"/>
+            <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Search') ?>"/>
+            <button><i class="fa fa-search" aria-hidden="true"></i></button>
+          </form>
+        </div>
   		</div>
   	</div>
   </div>
