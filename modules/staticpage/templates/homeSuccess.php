438198d85c7b1cf3b21b7ed203eba54b79f761dd
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-10-21 12:52:45 -0400

    mask

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -18,7 +18,7 @@
 	  <li data-target="#myCarousel" data-slide-to="2" class=""></li>
 	</ol>
 	<div class="carousel-inner">
-	  <div class="item active" style="background-image: url('https://fit.vrchost.com/media/get/77122/lb_sc_000150/');">
+	  <div class="item active" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://fit.vrchost.com/media/get/77122/lb_sc_000150/');">
 		<div class="carousel-caption">
 		  <div class ="container">
 			  <a href="http://192.168.99.100/index.php/arsho-baghsarian-shoe-sketches-1963-2008"><h4>First Thumbnail label</h4>
@@ -26,7 +26,7 @@
 		  </div>
 		</div>
 	  </div>
-	  <div class="item" style="background-image: url('https://fit.vrchost.com/media/get/66811/ls0001406/');">
+	  <div class="item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://fit.vrchost.com/media/get/66811/ls0001406/');">
 		<div class="carousel-caption">
 		  <div class ="container">
 			  <a href="http://192.168.99.100/index.php/arsho-baghsarian-shoe-sketches-1963-2008"><h4>Second Thumbnail label</h4>
@@ -34,7 +34,7 @@
 		  </div>
 		</div>
 	  </div>
-	  <div class="item" style="background-image: url('https://fit.vrchost.com/media/get/76636/lb_sc_000065/');">
+	  <div class="item" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://fit.vrchost.com/media/get/76636/lb_sc_000065/');">
 		<div class="carousel-caption">
 		<div class ="container">
 			  <a href="http://192.168.99.100/index.php/arsho-baghsarian-shoe-sketches-1963-2008"><h4>Third Thumbnail label</h4>
