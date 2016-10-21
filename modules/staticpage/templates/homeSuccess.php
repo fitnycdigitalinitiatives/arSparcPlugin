fad402221f536a0e825617e735b3f0ed24931e43
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-10-21 12:30:58 -0400

    homepage

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -6,7 +6,7 @@
     <h2>The archival collections of the FIT Library's Special Collections and College Archives</h2>
     <form action="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse')) ?>" data-autocomplete="<?php echo url_for(array('module' => 'search', 'action' => 'autocomplete')) ?>" autocomplete="off">
       <input type="hidden" name="topLod" value="0"/>
-      <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Find Archival Materials') ?>"/>
+      <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo $sf_request->query ?>" placeholder="<?php echo __('Connect to the Collection') ?>"/>
       <button><i class="fa fa-search" aria-hidden="true"></i></button>
     </form>
   </div>
