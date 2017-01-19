084c3a404a49a087477fd23dacfd19cdff78b0bc
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-01-19 12:27:56 -0500

    language dropdown

--- modules/menu/templates/_changeLanguageMenu.php
+++ modules/menu/templates/_changeLanguageMenu.php
@@ -2,7 +2,7 @@
 
 	  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('Language') ?><b class="caret"></b></a>
 
-		<ul>
+		<ul class="dropdown-menu">
 		  <?php foreach ($langCodes as $value): ?>
 		    <li<?php if ($sf_user->getCulture() == $value): ?> class="active"<?php endif; ?>>
 		      <?php echo link_to(format_language($value, $value), array('sf_culture' => $value) + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()) ?>
