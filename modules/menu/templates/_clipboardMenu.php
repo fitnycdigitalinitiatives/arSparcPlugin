c3643fe2ef1937cd19d5f40cf18effce7458698c
Joseph Anderson <fitnycdigitalinitiatives@users.noreply.github.com>
2020-01-22 14:40:54 -0500

    updating clipboard button

--- modules/menu/templates/_clipboardMenu.php
+++ modules/menu/templates/_clipboardMenu.php
@@ -1,6 +1,8 @@
 <li class="dropdown" id="clipboard-menu" data-toggle="tooltip" data-title="<?php echo __('Clipboard') ?>" data-clipboard-status-url="<?php echo url_for(array('module' => 'user', 'action' => 'clipboardStatus')) ?>" data-alert-message="<?php echo __('Note: clipboard items unclipped in this page will be removed from the clipboard when the page is refreshed. You can re-select them now, or reload the page to remove them completely. Using the sort or print preview buttons will also cause a page reload - so anything currently deselected will be lost!') ?>">
 
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo __('Clipboard') ?><?php if ($count > 0): ?> <span class="badge"><?php echo $count ?></span><?php endif; ?></a>
+	<button class="dropdown-toggle" data-toggle="dropdown" data-target="#" aria-expanded="false">
+    <?php echo __('Clipboard') ?>
+  </button>
 
 	<ul class="dropdown-menu">
     <?php echo QubitMenu::displayHierarchyAsList($menu, 0, array('overrideVisibility' => array('admin' => $sf_user->isAdministrator()))) ?>
